537c988c13403e539b9966f5c6312a40588a906b
armaBurton <armanarma@armaburton.com>
2022-03-15 19:29:27 -0700

    second test passes, app is now able to create an object

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,8 +13,28 @@ describe('alchemy-app routes', () => {
     pool.end();
   });
 
+  it('should be able to create a new instance of a ship', async () => {
+    const res = await request(app)
+      .post('/api/v1/star_trek_ships')
+      .send({
+        name: 'Cube 461',
+        faction: 'Borg',
+        class_specifications: 'Cube Variant 2',
+        crew_compliment: 129000,
+        length_meters: 3000
+      });
 
-  it('should be able to get all space ships', async () => {
+    expect(res.body).toEqual({
+      name: 'Cube 461',
+      faction: 'Borg',
+      id: expect.any(String),
+      class_specifications: 'Cube Variant 2',
+      crew_compliment: 129000,
+      length_meters: 3000
+    });
+  });
+
+  it('should be able to list all space ships', async () => {
     const expected = await Ship.findAll();
     const res = await request(app)
       .get('/api/v1/star_trek_ships');
